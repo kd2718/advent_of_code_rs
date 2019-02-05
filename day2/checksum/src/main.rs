ecb7ea4d282f0a92d7590095f18a5afc20a6fcaa
kd2718 <korydonati@gmail.com>
2019-02-04 22:44:08 -0800

    changed .gitignore

--- day2/checksum/src/main.rs
+++ day2/checksum/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> Result<(),std::io::Error> {
+    let mut file = File::create("input.txt")?;
+    //let file = file!("../data/input.txt");
+
+
+    let mut input = vec![];
+    file.read_to_end(&mut input)?;
+    for line in input {
+        println!("{}", line)
+    }
+
+    Ok(())
 }
